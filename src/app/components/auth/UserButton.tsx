bf9cf44d4f80a732b30ed2ecdcd654ebf1bea59e
Shahid Ali <shahidbaloch.sm@gmail.com>
2025-07-06 00:08:00 +0500

    Create UserButton.tsx

---
+++ src/app/components/auth/UserButton.tsx
@@ -0,0 +1,7 @@
+export const UserButton = () => {
+  return (
+    <div className="flex items-center space-x-4">
+      <span className="text-white">Welcome to SKSM Trade!</span>
+    </div>
+  );
+};
