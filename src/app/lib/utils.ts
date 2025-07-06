da1e08730d8fa5850d7a76b49ee48424b8ae82dd
Shahid Ali <shahidbaloch.sm@gmail.com>
2025-07-06 19:05:59 +0500

    Delete utils.ts

--- src/app/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-
-// export function cn(...inputs: ClassValue[]) {
-  // return twMerge(clsx(inputs))
-// }
-// Utility function to concatenate class names
-export function cn(...classes: (string | undefined | false | null)[]): string {
-  return classes.filter(Boolean).join(' ');
-}
