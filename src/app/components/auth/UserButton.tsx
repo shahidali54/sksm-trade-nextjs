6eb36ffb991f2878d6db308a0aeb36d1a563165f
Shahid Ali <shahidbaloch.sm@gmail.com>
2025-07-06 19:06:51 +0500

    Delete UserButton.tsx

--- src/app/components/auth/UserButton.tsx
+++
@@ -1,7 +0,0 @@
-export const UserButton = () => {
-  return (
-    <div className="flex items-center space-x-4">
-      <span className="text-white">Welcome to SKSM Trade!</span>
-    </div>
-  );
-};
