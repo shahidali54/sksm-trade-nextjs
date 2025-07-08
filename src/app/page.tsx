26b7b07ee37e3a9055a2c0652befa026476940c0
Shahid Ali <shahidbaloch.sm@gmail.com>
2025-07-08 20:25:56 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,12 @@ import HowItWorks from "@/app/components/sections/HowItWorks";
 import SupportSection from "@/app/components/sections/SupportSection";
 import NewsSection from "@/app/components/sections/NewsSection";
 import Testimonials from "@/app/components/sections/Testimonials";
+import FAQSection from "@/app/components/sections/FAQSection";
 import Footer from "@/app/components/layout/Footer";
 import ScrollToTop from "@/app/components/layout/ScrollToTop";
 import LaunchCountdown from "@/app/components/sections/LaunchCountdown";
 import AnimatedBackground from "@/app/components/layout/AnimatedBackground";
+import ContactSection from "@/app/components/sections/ContactSection";
 
 export default function Home() {
   return (
@@ -29,6 +31,8 @@ export default function Home() {
       <SupportSection />
       <NewsSection />
       <Testimonials />
+      <FAQSection />
+      <ContactSection />
       <Footer />
       <ScrollToTop />
     </div>
